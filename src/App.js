19b0addba789455e8f2d13e00bc2201264d0b334
thebadmos <badmosolamiji@gmail.com>
2021-10-05 17:07:20 +0100

    intialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+   const [quote, setQuote] = useState('')
+   const [author, setAuthor] = useState('')
+  const getQuote = () =>{
+    axios.get('https://api.quotable.io/random')
+    .then(res =>{
+     console.log(res.data.content)
+     setQuote(res.data.content)
+     setAuthor(res.data.author)
+    }).catch(err =>{
+     console.log(err)
+    })
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={getQuote}>
+        Get Inspired</button>
+        <div className="quote">
+       { quote && <p>{quote}</p> }
+       </div>
+       <div className="author">
+      { author && <p className="auth"> ~{author}</p>}
+    </div>
     </div>
   );
 }
